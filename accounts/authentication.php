7033fcfbc56badd7afd57d6d04d674e20ebb7673
Francis Ong <francis.ong25@gmail.com>
2021-04-22 20:57:30 +1200

    Add authentication procedure -redirect to login apge if not logged in

---
+++ accounts/authentication.php
@@ -0,0 +1,11 @@
+<?php 
+
+	//start session
+	session_start();
+
+	// condition
+	if(isset($_SESSION['username'])){} // proceed to index html (log-in success)
+	else{header('Location: accounts/login.php');} // back to login page (log-in failed)
+	// end
+	
+?>
