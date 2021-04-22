2b310b36c0c4333d3c999d10487f93b3daa6de59
Francis Ong <francis.ong25@gmail.com>
2021-04-22 20:52:05 +1200

    Add require authentication.php

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html>
 <head>
 	<title>My Website | Francis Ong</title>
+	<?php require 'accounts/authentication.php'; ?>
 </head>
 <body>
 
+	<?php if (isset($_POST['destroy'])) {session_destroy();}?>
+
 	<h1>Welcome to My Website</h1>
+
+	<form method="post">
+		<button name="destroy">Destroy Session</button>
+	</form>
+	
 
 </body>
 </html>
